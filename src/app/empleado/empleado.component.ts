3303f02d40060a57f25e3d67ef7bae5e7e333787
ivan2003sr <ivan_2003sr@hotmail.com>
2021-03-05 19:03:25 -0300

    Event Binding video 10

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -27,9 +27,25 @@ llamaEmpresa(value:String){
 
   usuRegistrado=true;
 
+  textoDeRegistro="No hay nadie registrado";
+
 getRegistroUsuario(){
 
-  this.usuRegistrado=true;
+  this.usuRegistrado=false;
+}
+
+setUsuarioRegistrado(event:Event){
+  //alert("El usuario se acaba de registrar");
+  //this.textoDeRegistro="El usuario se acaba de registrar";
+  //alert(event.target);
+
+  if((<HTMLInputElement>event.target).value=="si"){
+
+    this.textoDeRegistro="El usuario se acaba de registrar";
+  }else{
+    this.textoDeRegistro="No hay nadie registrado";
+  }
+
 }
 
 
